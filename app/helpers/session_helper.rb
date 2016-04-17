4b2ac8779ec3e5439eafbc8c30a58732ed04aaa5
arsh <mafzhussain@gmail.com>
2016-04-17 16:02:24 +0530

    logout and login working fine

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -4,6 +4,24 @@ module SessionHelper
   def login(user)
     session[:user_id] = user.id
   end
+  #Remembers the user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  #Returns the user corresponding to the remember token
+  def create
+    if (user_id = session[:user_id])
+      @current_user = User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+  end      
 # Returns the user 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -12,8 +30,15 @@ module SessionHelper
   def logged_in?
     !current_user.nil?
   end
+# Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 # Logs out the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end       
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,35 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password    
-  validates :password, presence: true, length: { maximum: 18 }
+  validates :password, length: { minimum: 6 }
+  
+  #Returns the hash digest of the given string
+  def User.digest(string)
+    cost = BCrypt::Engine::MIN_COST
+    BCrypt::Password.create(string, cost: cost)
+  end     
+  #forgets an user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  #Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end  
+  #Remembers the user in the database for use in a persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end                                             
+  #Returns true if the given token matches with remember_digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
