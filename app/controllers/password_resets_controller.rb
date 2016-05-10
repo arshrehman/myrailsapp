1551a9b26f7fb200a18990393c7a6674f66b24a3
arshrehman <mafzhussain@gmail.com>
2016-05-10 23:49:40 +0530

    Finished with user password reset and account activation email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
   
   def new
   end
@@ -21,7 +22,25 @@ class PasswordResetsController < ApplicationController
   def edit
   end
   
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "Can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      login @user
+      flash[:success] = "Password has been successfuly reset"
+      redirect_to @user  
+    else
+      render 'edit'
+    end
+  end     
+  
   private
+     
+     #required/allowed params
+     def user_params
+       params.require(:user).permit(:password, :password_confirmation)
+     end  
   
      def get_user
        @user = User.find_by(email: params[:email])
@@ -32,5 +51,13 @@ class PasswordResetsController < ApplicationController
        unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
        redirected_to root_url
        end
-     end     
+     end 
+     #checks expiration of the reset token
+     def check_expiration
+       if @user.password_reset_expired?
+         flash[:danger] ="Password reset link has been expired"
+         redirect_to new_password_reset_url
+       end
+     end    
+             
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,11 @@ class User < ActiveRecord::Base
   #Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end  
+  
+  #Returns true if password reset link expired
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end        
   private
     # Converts email to all lower case
